96ac0f0ba7cde11a20b9a24d7b79d696cdab7430
Ilativ <106491127+Ilativ@users.noreply.github.com>
2022-06-04 23:46:18 +0200

    Implemented title screen

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject[] preyPrefabs;
     public GameObject[] predatorPrefabs;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
 
     public void SpawnPrey()
     {
